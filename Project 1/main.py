1cac46ddc1920db69a882661883ddcd3acdfc913
Alph702 <147134651+Alph702@users.noreply.github.com>
2023-10-19 21:13:11 +0500

    On branch main     modified:   Project 1/main.py

--- Project 1/main.py
+++ Project 1/main.py
@@ -1,22 +1,31 @@
 # project is conter
-
+from time import sleep
 def deposet():
-    try:
-        while True:
-            amount = input("What amount the prodect is? $")
-            if amount.isdigit:
-                amount = int(amount)
-                if amount > 0:
-                    break
-                else:
-                    print("The velue is grater then 0.")
+    while True:
+        amount = input("What amount the prodect is? $")
+        if amount.isdigit:
+            amount = int(amount)
+            if amount > 0:
+                break
             else:
-                print("Plese enter a number.")
-    except Exception as e:
-        print(e)
+                print("The velue is grater then 0.")
+        else:
+            print("Plese enter a number.")
+    return amount
 
 def Main():
     while True:
-        thing = input("What the thing is: ")
-        things = []
-        while True
+        thing = input("What the thing is (q for quit): ")
+        if thing != 'q':
+            things = []
+            things.append(thing)
+
+            price = deposet()
+            prices = []
+            prices.append(price)
+
+            print(f"The price of {thing} is {price}")
+        else:
+            print(f"things is {things} price is {prices}")
+            sleep(60)
+            break
